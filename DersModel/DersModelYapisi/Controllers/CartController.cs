f4c4c50efe3c42f910fdcd510333e12a69434ad0
Almıla <almilaoztaskin@gmail.com>
2023-07-04 02:26:11 +0300

    Görev 8

---
+++ DersModel/DersModelYapisi/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using DersModelYapisi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DersModelYapisi.Controllers
+{
+	public class CartController : Controller
+	{
+		public IActionResult Index()
+		{
+			var product1 = new ProductViewModel { Id = 1, Name = "Product 1", Price = 10 };
+			var product2 = new ProductViewModel { Id = 2, Name = "Product 2", Price = 20 };
+			var product3 = new ProductViewModel { Id = 3, Name = "Product 3", Price = 30 };
+
+			var cartProduct1 = new CartProductViewModel { Id = 1, Product = product1, Quantity = 10, Price = 5 };
+			var cartProduct2 = new CartProductViewModel { Id = 2, Product = product2, Quantity = 7, Price = 8 };
+			var cartProduct3 = new CartProductViewModel { Id = 3, Product = product3, Quantity = 3, Price = 15 };
+
+			var cartProducts = new List<CartProductViewModel> { cartProduct1, cartProduct2, cartProduct3 };
+
+
+			return View(cartProducts);
+		}
+	}
+}
---
+++ DersModel/DersModelYapisi/Models/CartProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DersModelYapisi.Models
+{
+	public class CartProductViewModel
+	{
+		public int Id { get; set; }
+		public ProductViewModel? Product {get; set;}
+		public int Quantity { get; set;}
+		public decimal Price { get; set;}
+	}
+}
